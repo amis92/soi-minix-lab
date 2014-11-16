f559514711028a65c39fa7df296363d1d6c73200
Amadeusz Sadowski <amis.tau.92@gmail.com>
2014-11-16 23:26:40 +0100

    tests cleaned, header added

--- sched_long.c
+++ sched_long.c
@@ -1,10 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <lib.h>
+
+#include "scheduling.h"
+
+
 #define TRUE 1
 
 
 int main(int argc, char* argv[])
 {
+  pid_t pid = getpid();
+  char group;
   int index, result;
   result = 1;
+  
+  if (argc != 2)
+  {
+    printf("> Usage: %s <sched group:A/B/C>\n", argv[0]);
+    return 0;
+  }
+  
+  
+  group = getgroup(pid);
+  printf("> Process pid=%d (this) is of group '%c'\n", pid, group);
+  
+  group = argv[1][0];
+  if (group <'A' || group > 'C')
+  {
+    printf("> Invalid group code. Only A,B or C is allowed\n");
+    return 0;
+  }
+  
+  
+  printf("> Changing group to '%c'...\n", group);
+  setgroup(pid, group);
+  
+  group = getgroup(pid);
+  printf("> Process pid=%d (this) is now of group '%c'\n", pid, group);
+  
   while (TRUE)
   {
     result = result * index;
